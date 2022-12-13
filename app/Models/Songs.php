5eb4128c3c4a60c79d9a685277769d9aaf61a797
lati111 <jvzelst111@gmail.com>
2022-12-13 09:28:59 +0100

    Added song table to database

---
+++ app/Models/Songs.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Songs extends Model
+{
+    use HasFactory;
+    protected $table = 'song';
+    protected $primaryKey = 'id';
+}
---
+++ database/migrations/2022_12_13_082225_create_song_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('song', function (Blueprint $table) {
+            $table->id();
+            $table->string("song_name", 30);
+            $table->string("author", 30);
+            $table->year("release_year");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('song');
+    }
+};
